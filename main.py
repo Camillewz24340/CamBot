36a4b459f953fb8498305e6a3a5c8db2afba5779
BuildTools <unconfigured@null.spigotmc.org>
2022-07-25 19:29:08 +0200

    emojis answers

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from random import random
 import discord
 import os
 import random
-
+import time
 token = open("./token.txt", "r")
 
 client = discord.Client()
@@ -13,14 +13,24 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    
     if message.author == client.user:
         return
-
     msg = message.content.lower()
+    send = message.channel.send
 
-    if msg == "hi" or msg == "hello" or msg == "cam hello" or msg == "cam hi":
+    if msg == "hi" or msg == "hello" or msg == "cam hello" or msg == "cam hi" or msg == "hey" or msg == "cam hey":
         messages = ["Hi !","Hi there !","Hey ! how it's going ?","Yo ! Whats up ?","Hey !","Hello !","Oh, hi !"]
         randMessage = random.randint(0,len(messages) - 1)
-        await message.channel.send(messages[randMessage])
+        await send(messages[randMessage])
+
+    if msg.startswith("hm"):
+        await send(":thinking:")
+    
+    if msg.startswith("lol"):
+        await send(":joy:")
+        
+    if msg.startswith("aa") or msg.startswith("ahh"):
+        await send(":scream:")
 
 client.run(token.read())
