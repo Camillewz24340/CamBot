f36f95ce69a78815c060124c98ab940e8cd9378e
BuildTools <unconfigured@null.spigotmc.org>
2022-07-26 19:55:29 +0200

    improved debugging and cmd window

---
+++ colors.py
@@ -0,0 +1,7 @@
+class Colors:
+    FATAL = "red"
+    ERROR = "red"
+    DURINGOPERATION = "white"
+    WARNING = "orange"
+    SUCCESS = "green"
+    INFO = "white"
--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-from random import random
+import imp
+import random
+import re
+from turtle import title
 import discord
 import os
-import random
 import time
-token = open("./token.txt", "r")
+from termcolor import colored as col3
+from colors import Colors
 
+root.iconbitmap('icon.ico')
+os.system("cls")
+os.system("title CamBot - Starting")
+
+token = open("./token.txt", "r")
+OwnerID = 660631616074547224
 client = discord.Client()
+
+print(col3("Starting", Colors.DURINGOPERATION))
 
 @client.event
 async def on_ready():
-    print("Ready")
+    os.system("cls")
+    print(col3("Ready", Colors.SUCCESS))
+    os.system("title CamBot - Ready")
 
 @client.event
 async def on_message(message):
@@ -27,10 +40,16 @@ async def on_message(message):
     if msg.startswith("hm"):
         await send(":thinking:")
     
-    if msg.startswith("lol"):
+    if re.match("lol|lmao|xd", msg):
         await send(":joy:")
         
     if msg.startswith("aa") or msg.startswith("ahh"):
         await send(":scream:")
+
+    if msg == "cam restart" and message.author.id == OwnerID:
+        print(col3("[INFO] Restart Request Detected", Colors.INFO))
+        os.system('cmd /k "python main.py"')
+        os.kill()
+
 
 client.run(token.read())
